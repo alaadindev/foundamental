ce2a102f4bee30c0421b38e3bc75f63c84b52b13
alaadev <alaadindev@gmail.com>
2025-09-30 13:12:02 +0300

    added heapsort bubble down(standard)

--- 2-sorting/comparison-sorts/heap-sort/heap_sort.py
+++ 2-sorting/comparison-sorts/heap-sort/heap_sort.py
@@ -20,4 +20,27 @@ def heapfiy_up(arr, n):
         return heapfiy_up(arr, n)
     return arr
 
+def heapify(arr, n, i):
+    largest = i
+    left = 2*i +1
+    right = 2*i +2
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+    
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n//2-1, -1, -1):
+        heapify(arr, n, i)
+    
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+    return arr
+
 print(heap_sort_up([3, 2, 3, 7, 5, 6, 4]))
+print(heap_sort([3, 2, 3, 7, 5, 6, 4]))
