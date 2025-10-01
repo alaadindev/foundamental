5be679082822dd545f30b0177b5fe4165c3e4bc3
alaadev <alaadindev@gmail.com>
2025-10-01 11:54:14 +0300

    updated shell_sort with true shell sort instead of gap bubble

--- 2-sorting/hybrid-sort/shell-sort/shell_sort.py
+++ 2-sorting/hybrid-sort/shell-sort/shell_sort.py
@@ -10,4 +10,25 @@ def odd_shell_sort(arr):
         gap = gap//2
     return arr
 
-print(odd_shell_sort([3,2,6,1,7,4,9]))
+#still different then the standard but shell sort this time
+def shell_sort(arr):
+    n = len(arr)
+    gap = n//2
+    while gap > 0:
+        for i in range(gap):
+            for j in range(i+gap, n, gap):
+                temp = arr[j]
+                last_pos = j
+                for k in range(j-gap, -1, -gap):
+                    if arr[k] > temp:
+                        arr[k+gap] = arr[k]
+                        last_pos = k
+                    else:
+                        break
+                arr[last_pos] = temp
+        gap = gap//2
+    return arr
+
+print(shell_sort([3,2,0,1,5,4,8,3,4,6,1,7,4,9]))
+
+#print(odd_shell_sort([3,2,6,1,7,4,9]))
