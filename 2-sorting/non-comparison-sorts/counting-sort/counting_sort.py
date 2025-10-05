515009dbcc168662b00e3e621599f3e811417f62
alaadev <alaadindev@gmail.com>
2025-10-05 23:15:51 +0300

    counting loops setup

--- 2-sorting/non-comparison-sorts/counting-sort/counting_sort.py
+++ 2-sorting/non-comparison-sorts/counting-sort/counting_sort.py
@@ -1,8 +1,16 @@
 def counting_sort(arr):
     len = max(arr) - min(arr)
     count = [0]*(len)
-    for i in range(arr-1):
+    for i in range(len(arr)-1):
         count[i] += 1
+    acc = 0
+    for i in range(len(count)-1):
+        acc += count[i]
+        count[i] = acc 
+    for i in range(count-1, 0, -1):
+        count[i] = count[i-1]
+    count[0] = 0
+    print(count)
     
     return arr
 
