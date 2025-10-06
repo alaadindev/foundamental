3894569bec61cb696b5a54dba92ee81c948ec926
alaadev <alaadindev@gmail.com>
2025-10-06 12:57:43 +0300

    first full working counting sort

--- 2-sorting/non-comparison-sorts/counting-sort/counting_sort.py
+++ 2-sorting/non-comparison-sorts/counting-sort/counting_sort.py
@@ -1,19 +1,27 @@
 def counting_sort(arr):
-    len = max(arr) - min(arr)
-    count = [0]*(len)
-    for i in range(len(arr)-1):
-        count[i] += 1
+    count_range = max(arr) - min(arr)
+    count = [0]*(count_range+1)
+    print(arr)
+    for j in range(len(arr)):
+        count[arr[j]] +=1
     acc = 0
-    for i in range(len(count)-1):
+    print(count)
+    for i in range(count_range):
         acc += count[i]
         count[i] = acc 
-    for i in range(count-1, 0, -1):
+    print(count)
+    for i in range(count_range, 0, -1):
         count[i] = count[i-1]
     count[0] = 0
     print(count)
-    
-    return arr
+    newarr =[0] * len(arr)
+    for i in range(len(newarr)):
+        newarr[count[arr[i]]] = arr[i]
+        count[arr[i]] += 1
+    print(count)
+
+    return newarr
 
 
 
-print(counting_sort([2,0,8,3,9,5,8,7,1]))
+print(counting_sort([1,0,3,1,3,1]))
